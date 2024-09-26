d5bd9aba86a63cd9b2d7b9ee7dd8104d92bab8ed
rafaelgrando <sta_rafinha@hotmail.com>
2024-09-25 22:15:08 -0300

    feat: feito alguns ajustes no arquivo createTablesDB.sql. E no arquivo SelectComFiltrosOrdenacaoEPaginacao.sql foi adicionado comandos 'case' e consultas com Join

--- query/SelectComFiltrosOrdenacaoEPaginacao.sql
+++ query/SelectComFiltrosOrdenacaoEPaginacao.sql
@@ -27,6 +27,25 @@ select id, nome, cpf from estudantes where lower(nome) = 'RAFAEL GRANDO'; # lowe
 select id, nome, cpf from estudantes where lower(nome) = lower('RAFAEL GRANDO');
 SELECT * FROM estudantes;
 SELECT * FROM turmas;
-select * from turmas where year(data_inicio) = 2025; # pega o ano da data de inicio
 
+select * from turmas where year(data_inicio) = 2025; # pega o ano da data de inicio
+select * from turmas where year(data_inicio) = 2023 and month(data_inicio) = 1 and day(data_inicio) = 10;
 SELECT * FROM turmas WHERE data_inicio = str_to_date('10/01/2023', '%d/%m/%Y'); # converter usando uma máscara especifica
+
+#     ----------------------------------------------
+# Comando case no sql
+desc estudantes;
+select * from estudantes;
+select id, nome, case when endereco is null then '(not registered)' else endereco end from estudantes;
+select id, nome, case when endereco like '%Rua A%' then '(not registered)' else endereco end from estudantes;
+
+# ------------------------------------------------------
+# Consultas com JOIN no SQL
+select * from cursos join turmas; #isso não é recomendado, pois não tem um critério de junção
+select * from cursos join turmas on cursos.id = turmas.curso_id; # isso é o correto
+select * from cursos inner join turmas on cursos.id = turmas.curso_id;  # inner join é o padrão do join voce usa o inner quando quer fazer a consulta de uma tabela juntando o registro da outra tabela mas só quer trazer os registros que batem a interseção
+select * from cursos left join turmas on cursos.id = turmas.curso_id; #traga todos as cursos inclusive os que nao tem turmas
+select * from cursos right join turmas on cursos.id = turmas.curso_id; #traga todos as turmas inclusive as que não tem cursos
+
+select * from cursos left join turmas on cursos.id = turmas.curso_id where periodo = 'NOTURNO';
+select cursos.codigo, turmas.data_inicio, turmas.data_fim from cursos left join turmas on cursos.id = turmas.curso_id where periodo = 'MATUTINO' order by data_inicio;
